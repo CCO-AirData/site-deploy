86dd5953252b67b01d6928cddfe5f60366405014
HugoHanashiro <hugo.galdino@sptech.school>
2022-12-01 22:57:13 -0300

    fix: dados na dash quando é acessada por qr code

--- site/src/models/processosModel.js
+++ site/src/models/processosModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function listarProcessos(fkTorre, limite, fkServidor) {
     if(process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         var instrucao = `SELECT nome, sum(porcentagemCpu) as 'cpu', pid, usuario FROM processos 
-        WHERE DAY(horario) >= DAY(now()) AND HOUR(horario) >= HOUR(now()) AND MINUTE(horario) >= MINUTE(now())
+        WHERE DAY(horario) >= DAY(now()) AND HOUR(horario) >= HOUR(now()) AND MINUTE(horario) >= MINUTE(now()) AND SECOND(horario) >= SECOND(now()-3)
         AND fkServidor = "${fkServidor}"
         GROUP BY nome ORDER BY sum(porcentagemCpu) DESC LIMIT ${limite};`;
         console.log("Executando a instrução SQL: \n" + instrucao);
@@ -15,8 +15,14 @@ function listarProcessos(fkTorre, limite, fkServidor) {
 }
 
 function deletarProcesso(pid) {
-    var instrucao = `INSERT INTO deletarPid(pid) values (${pid});`
-    return database.executar(instrucao);
+    if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        var instrucao = `INSERT INTO deletarPid(pid) values (${pid});`
+        console.log("Executando a instrução SQL: \n" + instrucao);    
+    } else if(process.env.AMBIENTE_PROCESSO == "producao"){
+        var instrucao = `INSERT INTO deletarPid(pid) values (${pid});`
+        console.log("Executando a instrução SQL: \n" + instrucao);    
+    }
+        return database.executar(instrucao);
 }
 
 module.exports = {
